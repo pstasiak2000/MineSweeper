4b1a54dea1a033d998201cc3e0aaa642e84d420a
Piotr <p.stasiak@newcastle.ac.uk>
2026-03-30 20:27:53 +0100

    Added Debian installer file

---
+++ scripts/install_deb.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+
+REPO="pstasiak2000/MineSweeper"
+API_URL="https://api.github.com/repos/$REPO/releases/latest"
+
+echo "🔍 Fetching latest release..."
+
+DEB_URL=$(curl -s "$API_URL" \
+  | grep browser_download_url \
+  | grep amd64.deb \
+  | cut -d '"' -f 4)
+
+if [ -z "$DEB_URL" ]; then
+    echo "❌ Failed to find .deb file"
+    exit 1
+fi
+
+TMP_FILE=$(mktemp --suffix=.deb)
+
+echo "⬇️ Downloading package..."
+curl -L "$DEB_URL" -o "$TMP_FILE"
+
+echo "DEB URL: $DEB_URL"
+echo "TMP FILE: $TMP_FILE"
+
+#echo "📦 Installing..."
+#sudo apt install -y "$TMP_FILE"
+#
+#echo "🧹 Cleaning up..."
+#rm "$TMP_FILE"
+
+echo "✅ Installation complete!"
